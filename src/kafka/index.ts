68d367af6f1579955fcb308f7d8959166975af8d
Farid Mansimli <mensimli222@gmail.com>
2022-10-15 23:30:11 +0400

    kafka:config

--- src/kafka/index.ts
+++ src/kafka/index.ts
@@ -1,11 +1,12 @@
 import kafka from "./client";
 
-export class KafkaManager {
+export class MyKafka {
   static async initialize() {
     let admin;
     try {
       admin = kafka.admin();
       await admin.connect();
+
       await admin.createTopics({
         topics: [
           { topic: "logs", numPartitions: 1 },
@@ -31,6 +32,51 @@ export class KafkaManager {
       return Promise.reject(error);
     } finally {
       admin?.disconnect();
+    }
+  }
+
+  static async getProducer() {
+    let producer;
+    try {
+      producer = kafka.producer();
+      await producer.connect();
+      const result = await producer.send({
+        topic: "logs",
+        messages: [
+          { value: "message 1 to logs", partition: 0 },
+          { value: "message 2 to logs", partition: 1 },
+        ],
+      });
+      return producer;
+    } catch (error) {
+      return Promise.reject(error);
+    } finally {
+      producer?.disconnect();
+    }
+  }
+
+  static async getConsumer() {
+    let consumer;
+    try {
+      consumer = kafka.consumer({
+        groupId: "test_cg1",
+      });
+
+      await consumer.subscribe({
+        topic: "logs",
+        fromBeginning: true,
+      });
+
+      await consumer.run({
+        eachMessage: async result => {
+          console.log(`${result.message.value}=>partition:${result.partition}`);
+        },
+      });
+      return consumer;
+    } catch (error) {
+      return Promise.reject(error);
+    } finally {
+      consumer?.disconnect();
     }
   }
 }
